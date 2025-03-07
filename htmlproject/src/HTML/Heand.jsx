e51616556f54bd7b057beb604ecf63db328a7b5f
Shabanashanavas14 <shabanashanavas0510@gmail.com>
2025-03-07 10:49:48 +0530

    changed

--- htmlproject/src/HTML/Heand.jsx
+++ htmlproject/src/HTML/Heand.jsx
@@ -7,7 +7,8 @@ export default function Heand() {
       <header>
           <div>
             <h1><u>Explore our amazing tour</u></h1>
-            <p>Explore amazing destinations around the world. Discover the world-exclusive travel packages.</p>
+            <h4>Hello world</h4>
+            <p>i explore the travles.Explore amazing destinations around the world. Discover the world-exclusive travel packages.</p>
       <img src={travel} alt="" />
           </div>
         </header>
